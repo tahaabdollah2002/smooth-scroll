b05ae7cf59f1e52272558da064e9563f0bfe64f4
Mohammad Taha Abdollah <75626161+tahaabdollah2002@users.noreply.github.com>
2022-07-14 02:08:22 +0430

    Add full source code.

---
+++ app.js
@@ -0,0 +1,27 @@
+function smoothScroll(target,duration){
+    var target = document.querySelector(target);
+    var targetPosition = target.getBoundingClientRect().top;
+    var startPosition = window.scrollY;
+    var distance = targetPosition - startPosition;
+    var startTime = null;
+    function animation(currentTime){
+        if(startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0,run);
+        if(timeElapsed < duration) requestAnimationFrame(animation);
+    }
+    function ease(t, b, c, d) {
+        if ((t /= d / 2) < 1) return c / 2 * t * t + b;
+        return -c / 2 * ((--t) * (t - 2) - 1) + b;
+    }
+    requestAnimationFrame(animation);    
+}
+var section1 = document.querySelector('.section1');
+var section2 = document.querySelector('.section2');
+section1.addEventListener('click', function (){
+    smoothScroll('.section2', 1000);
+});
+section2.addEventListener('click', function (){
+    smoothScroll('.section1', 1000);
+});
